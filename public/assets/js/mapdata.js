94952cc038d3d4c014169911bf6e53b23d0edfc3
rumahac <saefudin.rumahac@gmail.com>
2023-01-03 14:08:09 +0700

    first commit

---
+++ public/assets/js/mapdata.js
@@ -0,0 +1,241 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    state_description: "",
+    state_color: "#88A4BC",
+    state_hover_color: "#7d1418",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#E62129",
+    location_opacity: 1,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "circle",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+   
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    IDN1136: {
+      name: "Aceh"
+    },
+    IDN1185: {
+      name: "Kalimantan Timur"
+    },
+    IDN1223: {
+      name: "Jawa Barat"
+    },
+    IDN1224: {
+      name: "Jawa Tengah"
+    },
+    IDN1225: {
+      name: "Bengkulu"
+    },
+    IDN1226: {
+      name: "Banten"
+    },
+    IDN1227: {
+      name: "Jakarta Raya"
+    },
+    IDN1228: {
+      name: "Kalimantan Barat"
+    },
+    IDN1229: {
+      name: "Lampung"
+    },
+    IDN1230: {
+      name: "Sumatera Selatan"
+    },
+    IDN1231: {
+      name: "Bangka-Belitung"
+    },
+    IDN1232: {
+      name: "Bali"
+    },
+    IDN1233: {
+      name: "Jawa Timur"
+    },
+    IDN1234: {
+      name: "Kalimantan Selatan"
+    },
+    IDN1235: {
+      name: "Nusa Tenggara Timur"
+    },
+    IDN1236: {
+      name: "Sulawesi Selatan"
+    },
+    IDN1237: {
+      name: "Sulawesi Barat"
+    },
+    IDN1796: {
+      name: "Kepulauan Riau"
+    },
+    IDN1837: {
+      name: "Gorontalo"
+    },
+    IDN1930: {
+      name: "Jambi"
+    },
+    IDN1931: {
+      name: "Kalimantan Tengah"
+    },
+    IDN1933: {
+      name: "Irian Jaya Barat"
+    },
+    IDN381: {
+      name: "Sumatera Utara"
+    },
+    IDN492: {
+      name: "Riau"
+    },
+    IDN513: {
+      name: "Sulawesi Utara"
+    },
+    IDN538: {
+      name: "Maluku Utara"
+    },
+    IDN539: {
+      name: "Sumatera Barat"
+    },
+    IDN540: {
+      name: "Yogyakarta"
+    },
+    IDN554: {
+      name: "Maluku"
+    },
+    IDN555: {
+      name: "Nusa Tenggara Barat"
+    },
+    IDN556: {
+      name: "Sulawesi Tenggara"
+    },
+    IDN557: {
+      name: "Sulawesi Tengah"
+    },
+    IDN558: {
+      name: "Papua"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "-6.174444",
+      lng: "106.829444",
+      name: "Kabinda Jakarta",
+      description: "Mohamad Reza | Kabinda Jabar"
+    },
+    "1": {
+      lat: "-6.000",
+      lng: "106.816",
+      name: "Kabinda Jakarta",
+      description: "Mohamad Agus | Kabinda Jabar"
+    },
+    "2": {
+      lat: -6.254,
+      lng: 106.797,
+      name: "Kabinda Jakarta",
+      description: "Alsyad Ahmad | Kabinda Jabar"
+    },
+    "3": {
+      lat: -0.495,
+      lng: 117.144,
+      name: "Kabinda Kalimantan",
+      description: "Emil Mutaqin | Kabinda Kalimantan"
+    },
+    "4": {
+      lat: -0.02,
+      lng: 109.336,
+      name: "Kabinda Kalimantan",
+      description: "Abang Obeng | Kabinda Kalimantan"
+    },
+    "5": {
+      lat: 2.763,
+      lng: 117.434,
+      name: "Kabinda Kalimantan",
+      description: "Tian Maulana | Kabinda Kalimatan"
+    },
+    "6": {
+      lat: -2.206,
+      lng: 113.912,
+      name: "Kabinda Kalimantan",
+      description: "Maulana | Kabinda Kalimantan"
+    },
+    "7": {
+      lat: 1.487,
+      lng: 124.842,
+      name: "Kabinda Sulawesi",
+      description: "Popo | Kabinda Sulawesi"
+    },
+    "8": {
+      lat: -3.972,
+      lng: 122.585,
+      name: "Kabinda Sulawesi",
+      description: "Sueb | Kabinda Sulawesi"
+    },
+    "9": {
+      lat: -5.146,
+      lng: 119.423,
+      name: "Kabinda Sulawesi",
+      description: "Gong | Kabinda Sulawesi"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
