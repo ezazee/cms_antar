3e752495d8452a0591c708762387f6dfcd11f98c
rumahac <saefudin.rumahac@gmail.com>
2023-01-11 11:58:07 +0700

    Footer

--- resources/views/master/footer.blade.php
+++ resources/views/master/footer.blade.php
@@ -2,10 +2,10 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-md-6">
-                                <script>document.write(new Date().getFullYear())</script> © NusantaraKu
+                                <script>document.write(new Date().getFullYear())</script> © PT EMAS
                             </div>
                             <div class="col-md-6">
-                                <div class="text-md-end footer-links d-none d-md-block">Design & Develop By NusantaraKu</div>
+                                <div class="text-md-end footer-links d-none d-md-block">Design & Develop By PT EMAS</div>
                             </div>
                         </div>
                     </div>
